f6c6e414ed8c82c6b8d6c9b41d40549abeedfddf
Guilherme Siquinelli <guiseek@gmail.com>
2023-10-28 18:17:37 -0300

    chore(migrate to nx v17): updated nx from 16 to 17 and angular packag… (#25)
    * chore(migrate to nx v17): updated nx from 16 to 17 and angular packages from 16 to 16.2
    
    * style(util-core): lint fix Function
    
    fixed error lint in use Function type
    
    * test(feature): fix test module import animations

--- libs/feature/src/lib/feature.component.spec.ts
+++ libs/feature/src/lib/feature.component.spec.ts
@@ -2,9 +2,10 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FeatureComponent } from './feature.component';
 import { MATERIAL_MODULES } from './material-modules';
 import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserTestingModule } from '@angular/platform-browser/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { SidenavMenuComponent } from './components';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('FeatureComponent', () => {
   let component: FeatureComponent;
@@ -13,8 +14,9 @@ describe('FeatureComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        BrowserAnimationsModule,
         MATERIAL_MODULES,
+        NoopAnimationsModule,
+        BrowserTestingModule,
         RouterTestingModule,
         ReactiveFormsModule,
       ],
